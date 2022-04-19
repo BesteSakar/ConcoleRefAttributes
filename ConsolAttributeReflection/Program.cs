c2cd3120aa95d3d7b7bd0d672704b926ecdffeaf
BesteSakar <bestesakarr@outlook.com>
2022-04-19 14:12:58 +0300

    Console Attributes Reflection
    Bu projede person ismindeki sınıfımıza tanımladığımız prop. hepsine değer girilmesini ve sonrasında bunları check metodumuzla(Control sınıfında) bu değerleri kontrol etme..

---
+++ ConsolAttributeReflection/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsolAttributeReflection
+{
+    internal class Program
+    {
+
+
+        /*
+         Attributeslar ve reflection genelde beraber kullanılırlar.
+        Run time esnasında class enum,method gibi yapılarla bilgi veren
+        ve kontrol etmemizi sağlayan yapılardır..
+        -------------------------------------------------------------
+
+        Aşağıdaki örneğimizde person ismindeki sınıfımıza tanımladığımız prop.lerin
+        hepsine bir değer girilmesini,oluşturduğumuz attributes satesinde zorunlu hale getirdik.
+        Control sınıfımızdaki check metodumuzlada bu kontrol işlemini sağladık..
+        Foreach ve iflerle doluluk durumuna bakıp,bool bir değer döndürdük...
+      
+
+        */
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            person p1 = new person()
+            {
+                Name = "Mehmet",
+                Age = 34
+
+            };
+
+            Console.WriteLine(Control.Check(p1));
+            Console.ReadLine();
+           
+        }
+
+        public class person
+        {
+            [zorunlu]
+            public string Name;
+            [zorunlu]
+            public string ID;
+            [zorunlu]
+            public int Age;
+        }
+
+
+        public class zorunluAttribute : Attribute
+        {
+
+        }
+
+        public static class Control
+        {
+            public static bool Check(person ins)
+            {
+                Type type = ins.GetType();
+                foreach(var item in type.GetFields())
+                {
+                    object[] attributes = item.GetCustomAttributes(typeof(zorunluAttribute), true);
+                    if(attributes.Length != 0)
+                    {
+                        object value = item.GetValue(ins);
+
+                        if(value == null)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+
+
+
+
+    }
+}
